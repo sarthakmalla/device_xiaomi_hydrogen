bb6285c4838fd4ea12c672b274eaaf8ad2b310d0
sarthakmalla <sarthakmalla17@gmail.com>
2018-07-16 19:25:03 +0530

    add rr official & smart pixel overlay

--- device.mk
+++ device.mk
@@ -70,3 +70,6 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from msm8956-common
 $(call inherit-product, device/xiaomi/msm8956-common/msm8956.mk)
+
+# FOR BUILDING OFFICIAL PACKAGE
+export export RR_BUILDTYPE=Official
--- rr.mk
+++ rr.mk
@@ -17,23 +17,18 @@
 
 $(call inherit-product, device/xiaomi/hydrogen/full_hydrogen.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+# Inherit some common rr stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := rr_hydrogen
 BOARD_VENDOR := Xiaomi
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := hydrogen
-PRODUCT_NAME := aicp_hydrogen
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Mi Max
-TARGET_VENDOR := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Use the latest approved GMS identifiers unless running a signed build
+ # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/hydrogen/hydrogen:6.0.1/MMB29M/V8.2.3.0.MBCCNDL:user/release-keys \
+    TARGET_DEVICE="hydrogen" \
+    PRODUCT_NAME="hydrogen" \
     PRIVATE_BUILD_DESC="hydrogen-user 6.0.1 MMB29M V8.2.3.0.MBCCNDL release-keys"
 endif
+
+BUILD_FINGERPRINT := Xiaomi/hydrogen/hydrogen:6.0.1/MMB29M/V8.2.3.0.MBCCNDL:user/release-keys
